24e2718beb28dd536358af58d8c1e9b6ce2a2de4
Harper <HarperMelissaM@gmail.com>
2025-09-15 01:37:39 -0500

    fix logic add documentation

--- customer-rewards/src/components/utils.jsx
+++ customer-rewards/src/components/utils.jsx
@@ -1,33 +1,48 @@
 import transactions from "../assets/transactions.json";
 
-export const calculateRewards = (arr) => {
+export const calculateRewards = (total) => {
   let totalPoints = 0;
-  for (let i of arr) {
-    if (i > 100) {
-      const over100 = i - 100;
-      totalPoints += over100 * 2 + 50;
-    } else if (i > 50) {
-      totalPoints += i - 50;
-    }
+  total = Math.round(total);
+  if (total > 100) {
+    const over100 = total - 100;
+    totalPoints += over100 * 2 + 50;
+  } else if (total > 50) {
+    totalPoints += total - 50;
   }
+
   return totalPoints;
 };
 
+/**
+ * Fetches all transactions made by the given customerId, calculates rewards points using the given criteria in README.md and returns an object of the total points for each month and total points overall
+ *
+ * @async
+ * @param {int} customerId
+ * @returns {unknown}
+ */
 export const getCustomerPoints = async (customerId) => {
   let data = transactions.filter(
     (transaction) => transaction.customerId === customerId
   );
-  let customerPoints = data.map((transaction) => {
+
+  let customerPoints = { totalPoints: 0 };
+
+  data.forEach((transaction) => {
     let transactionPoints = calculateRewards(transaction.total);
-    let month = transaction.date.getMonth();
-    if (`${month}Transactions` in customerPoints) {
-      let previousValue = customerPoints[`${month}Transactions`];
-      customerPoints[`${month}Transactions`] =
-        previousValue + transactionPoints;
+    let month = new Date(transaction.date).toLocaleString("default", {
+      month: "long",
+    });
+
+    // Add monthly points
+    if (month in customerPoints) {
+      customerPoints[month] += transactionPoints;
     } else {
-      customerPoints[`${month}Transactions`] = transactionPoints;
+      customerPoints[month] = transactionPoints;
     }
-    customerPoints.totalPoints += customerPoints;
+
+    // Add to total
+    customerPoints.totalPoints += transactionPoints;
   });
+
   return customerPoints;
 };
