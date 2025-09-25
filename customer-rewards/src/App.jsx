3df83c3c46adc6f27d348f18d4305b037c26c5df
Harper <HarperMelissaM@gmail.com>
2025-09-25 14:11:40 -0500

    remove unused code

--- customer-rewards/src/App.jsx
+++ customer-rewards/src/App.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Footer from "./components/Footer";
 import RewardsPage from "./pages/RewardsPage";
-import Home from "./pages/Home";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <NavBar />
       <Routes>
         <Route index element={<RewardsPage />} />
-        <Route path="home" element={<Home />} />
       </Routes>
       <Footer />
     </>
